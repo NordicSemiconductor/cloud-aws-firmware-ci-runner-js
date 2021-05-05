4fac09ec3b7ff3ca7cce4fa75a4a9430d2cef154
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-05-05 12:44:31 +0200

    fix: terminate job on read timeout

--- runner/runJob.ts
+++ runner/runJob.ts
@@ -53,99 +53,115 @@ export const runJob = async ({
 		)
 	}
 
-	progress(doc.id, `Connecting to ${device}`)
-	const { connection, deviceLog, onData } = await connect({
-		device: device,
-		atHostHexfile,
-		onEnd: async () => {
-			await flash({
-				hexfile: atHostHexfile,
-				...log('Resetting device with AT Host'),
-			})
-		},
-		...log(),
-	})
-	let flashLog: string[] = []
-	const { credentials } = doc
-	if (credentials !== undefined) {
-		progress(doc.id, 'Flashing credentials')
-		await flashCredentials({
-			...credentials,
-			...connection,
-		})
-	}
-	flashLog = await flash({
-		hexfile,
-		...log('Flash Firmware'),
-	})
-	progress(doc.id, `Setting timeout to ${doc.timeoutInMinutes} minutes`)
-	return new Promise((resolve) => {
+	return new Promise((resolve, reject) => {
 		let done = false
-		const t = setTimeout(async () => {
-			done = true
-			warn(doc.id, 'Timeout reached.')
-			await connection.end()
-			resolve({
-				result: { timeout: true, abort: false },
-				connection,
-				deviceLog,
-				flashLog,
-			})
-		}, doc.timeoutInMinutes * 60 * 1000)
+		progress(doc.id, `Connecting to ${device}`)
+		connect({
+			device: device,
+			atHostHexfile,
+			...log(),
+		})
+			.then(async ({ connection, deviceLog, onData, onEnd }) => {
+				let flashLog: string[] = []
+				const { credentials } = doc
 
-		const terminateOn = (type: string, result: Result, s: string[]) => {
-			progress(
-				doc.id,
-				`<${type}>`,
-				`Setting up ${type} traps. Job will terminate if output contains:`,
-			)
-			s?.map((s) => progress(doc.id, `<${type}>`, s))
-			const terminateCheck = allSeen(s)
-			onData(async (data) => {
-				s?.forEach(async (s) => {
-					if (data.includes(s)) {
-						warn(doc.id, `<${type}>`, 'Termination criteria seen:', s)
-					}
-				})
-				if (terminateCheck(data)) {
-					if (!done) {
+				progress(doc.id, `Setting timeout to ${doc.timeoutInMinutes} minutes`)
+				const jobTimeout = setTimeout(async () => {
+					done = true
+					warn(doc.id, 'Timeout reached.')
+					await connection.end()
+					resolve({
+						result: { timeout: true, abort: false },
+						connection,
+						deviceLog,
+						flashLog,
+					})
+				}, doc.timeoutInMinutes * 60 * 1000)
+
+				onEnd(async (_, timeout) => {
+					if (timeout) {
 						done = true
-						warn(
-							doc.id,
-							`<${type}>`,
-							'All termination criteria have been seen.',
-						)
-						clearTimeout(t)
-						await connection.end()
+						clearTimeout(jobTimeout)
+						warn(doc.id, 'Device read timeout occurred.')
 						resolve({
-							result,
+							result: { timeout: true, abort: false },
 							connection,
 							deviceLog,
 							flashLog,
 						})
 					}
+					await flash({
+						hexfile: atHostHexfile,
+						...log('Resetting device with AT Host'),
+					})
+				})
+				if (credentials !== undefined) {
+					progress(doc.id, 'Flashing credentials')
+					await flashCredentials({
+						...credentials,
+						...connection,
+					})
 				}
+				flashLog = await flash({
+					hexfile,
+					...log('Flash Firmware'),
+				})
+
+				const terminateOn = (type: string, result: Result, s: string[]) => {
+					progress(
+						doc.id,
+						`<${type}>`,
+						`Setting up ${type} traps. Job will terminate if output contains:`,
+					)
+					s?.map((s) => progress(doc.id, `<${type}>`, s))
+					const terminateCheck = allSeen(s)
+					onData(async (data) => {
+						s?.forEach(async (s) => {
+							if (data.includes(s)) {
+								warn(doc.id, `<${type}>`, 'Termination criteria seen:', s)
+							}
+						})
+						if (terminateCheck(data)) {
+							if (!done) {
+								done = true
+								warn(
+									doc.id,
+									`<${type}>`,
+									'All termination criteria have been seen.',
+								)
+								clearTimeout(jobTimeout)
+								await connection.end()
+								resolve({
+									result,
+									connection,
+									deviceLog,
+									flashLog,
+								})
+							}
+						}
+					})
+				}
+
+				if (doc.abortOn !== undefined)
+					terminateOn(
+						'abortOn',
+						{
+							abort: true,
+							timeout: false,
+						},
+						doc.abortOn,
+					)
+
+				if (doc.endOn !== undefined)
+					terminateOn(
+						'endOn',
+						{
+							abort: false,
+							timeout: false,
+						},
+						doc.endOn,
+					)
 			})
-		}
-
-		if (doc.abortOn !== undefined)
-			terminateOn(
-				'abortOn',
-				{
-					abort: true,
-					timeout: false,
-				},
-				doc.abortOn,
-			)
-
-		if (doc.endOn !== undefined)
-			terminateOn(
-				'endOn',
-				{
-					abort: false,
-					timeout: false,
-				},
-				doc.endOn,
-			)
+			.catch(reject)
 	})
 }
